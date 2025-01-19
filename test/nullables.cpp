153b71bebe555e6911c9709877fa8129bedfab9a
DNKpp <dnkpp2011@gmail.com>
2025-01-19 19:44:15 +0100

    chore: add NULLABLES_VERSION test

--- test/nullables.cpp
+++ test/nullables.cpp
@@ -1,6 +1,12 @@
-//          Copyright Dominic (DNKpp) Koepke 2024 - 2025.
+//          Copyright Dominic (DNKpp) Koepke 2025 - 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 #include "nullables/nullables.hpp"
+
+TEST_CASE("NULLABLES_VERSION is an integral number.")
+{
+    constexpr int version{NULLABLES_VERSION};
+    STATIC_REQUIRE(0 < version);
+}
