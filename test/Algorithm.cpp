ed2ddbbfe7df862f50314ae62c542c9dec80301f
Dominic Koepke <koepke@aunovis.de>
2025-10-08 20:39:39 +0200

    test: add cases for AndThenAlgorithm

--- test/Algorithm.cpp
+++ test/Algorithm.cpp
@@ -3,40 +3,38 @@
 namespace
 {
 
-
-
-        /*
-        template <typename Alternative>
-    struct value_or_algorithm
+    /*
+    template <typename Alternative>
+struct value_or_algorithm
+{
+    template <typename Nullable>
+    [[nodiscard]]
+    constexpr auto operator()(Nullable&& opt)
     {
-        template <typename Nullable>
-        [[nodiscard]]
-        constexpr auto operator()(Nullable&& opt)
+        if (detail::has_value(opt))
         {
-            if (detail::has_value(opt))
-            {
-                return std::invoke(*this, has_value_tag{}, std::forward<Nullable>());
-            }
-
-            return std::invoke(*this, is_empty_tag{}, std::forward<Nullable>());
+            return std::invoke(*this, has_value_tag{}, std::forward<Nullable>());
         }
 
-        template <typename Nullable>
-        [[nodiscard]]
-        constexpr auto operator()(detail::has_value_tag const, Nullable&& opt)
-        {
-            return value(std::forward<Nullable>(opt));
-        }
+        return std::invoke(*this, is_empty_tag{}, std::forward<Nullable>());
+    }
 
-        template <typename Nullable>
-        [[nodiscard]]
-        constexpr auto operator()(is_empty_tag const, [[maybe_unused]] Nullable&& opt)
-        {
-            return alternative;
-        }
+    template <typename Nullable>
+    [[nodiscard]]
+    constexpr auto operator()(detail::has_value_tag const, Nullable&& opt)
+    {
+        return value(std::forward<Nullable>(opt));
+    }
 
-        Alternative alternative;
-    };*/
+    template <typename Nullable>
+    [[nodiscard]]
+    constexpr auto operator()(is_empty_tag const, [[maybe_unused]] Nullable&& opt)
+    {
+        return alternative;
+    }
+
+    Alternative alternative;
+};*/
 
     template <typename... Steps>
     struct pipeline
@@ -64,11 +62,106 @@ namespace
     };
 }
 
-TEST_CASE("")
+namespace finally = mimicpp::finally;
+
+template <typename T>
+struct gimo::traits<std::optional<T>>
 {
-    /*pipeline{}
-        .append(AndThenAlgorithm<int>{})
-        .append(OrElseAlgorithm<int>{})
-        .append(TransformAlgorithm<int>{})
-        .append(value_or_algorithm<int>{});*/
+    static constexpr auto null{std::nullopt};
+};
+
+TEST_CASE(
+    "AndThenAlgorithm invokes its action when the input is non-empty."
+    "[algorithm]")
+{
+    mimicpp::Mock<
+        std::optional<int>(std::optional<float>)&,
+        std::optional<int>(std::optional<float>) const&,
+        std::optional<int>(std::optional<float>)&&,
+        std::optional<int>(std::optional<float>) const&&>
+        action{};
+
+    using Algorithm = gimo::AndThenAlgorithm<decltype(action)>;
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<float>, Algorithm&>);
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<float>, Algorithm const&>);
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<float>, Algorithm&&>);
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<float>, Algorithm const&&>);
+
+    SECTION("When input has a value, the action is invoked.")
+    {
+        constexpr std::optional opt{1337.f};
+        SECTION("When algorithm is used via lvalue-ref overload.")
+        {
+            SCOPED_EXP action.expect_call(1337.f)
+                and finally::returns(std::optional{42});
+            gimo::AndThenAlgorithm andThen{std::move(action)};
+            decltype(auto) result = andThen(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+
+        SECTION("When algorithm is used via const lvalue-ref overload.")
+        {
+            SCOPED_EXP std::as_const(action).expect_call(1337.f)
+                and finally::returns(std::optional{42});
+            gimo::AndThenAlgorithm andThen{std::move(action)};
+            decltype(auto) result = std::as_const(andThen)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+
+        SECTION("When algorithm is used via rvalue-ref overload.")
+        {
+            SCOPED_EXP std::move(action).expect_call(1337.f)
+                and finally::returns(std::optional{42});
+            gimo::AndThenAlgorithm andThen{std::move(action)};
+            decltype(auto) result = std::move(andThen)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+
+        SECTION("When algorithm is used via const rvalue-ref overload.")
+        {
+            SCOPED_EXP std::move(std::as_const(action)).expect_call(1337.f)
+                and finally::returns(std::optional{42});
+            gimo::AndThenAlgorithm andThen{std::move(action)};
+            decltype(auto) result = std::move(std::as_const(andThen))(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+    }
+
+    SECTION("When input is empty, action is not invoked.")
+    {
+        gimo::AndThenAlgorithm andThen{std::move(action)};
+        constexpr std::optional<float> opt{};
+
+        SECTION("When algorithm is used via lvalue-ref overload.")
+        {
+            decltype(auto) result = andThen(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(!result);
+        }
+
+        SECTION("When algorithm is used via const lvalue-ref overload.")
+        {
+            decltype(auto) result = std::as_const(andThen)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(!result);
+        }
+
+        SECTION("When algorithm is used via rvalue-ref overload.")
+        {
+            decltype(auto) result = std::move(andThen)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(!result);
+        }
+
+        SECTION("When algorithm is used via const rvalue-ref overload.")
+        {
+            decltype(auto) result = std::move(std::as_const(andThen))(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(!result);
+        }
+    }
 }
