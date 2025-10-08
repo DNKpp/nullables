6c3742e47c25619baa1406935f867306c2eaa246
Dominic Koepke <koepke@aunovis.de>
2025-10-08 21:13:16 +0200

    test: add cases for OrElseAlgorithm

--- test/Algorithm.cpp
+++ test/Algorithm.cpp
@@ -71,7 +71,7 @@ struct gimo::traits<std::optional<T>>
 };
 
 TEST_CASE(
-    "AndThenAlgorithm invokes its action when the input is non-empty."
+    "AndThenAlgorithm invokes its action only when the input has a value."
     "[algorithm]")
 {
     mimicpp::Mock<
@@ -162,6 +162,102 @@ TEST_CASE(
             decltype(auto) result = std::move(std::as_const(andThen))(opt);
             STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
             CHECK(!result);
+        }
+    }
+}
+
+TEST_CASE(
+    "OrElseAlgorithm invokes its action only when the input has no value."
+    "[algorithm]")
+{
+    mimicpp::Mock<
+        std::optional<int>()&,
+        std::optional<int>() const&,
+        std::optional<int>()&&,
+        std::optional<int>() const&&>
+        action{};
+
+    using Algorithm = gimo::OrElseAlgorithm<decltype(action)>;
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<int>, Algorithm&>);
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<int>, Algorithm const&>);
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<int>, Algorithm&&>);
+    STATIC_REQUIRE(gimo::detail::applicable_on<std::optional<int>, Algorithm const&&>);
+
+    SECTION("When input has no value, the action is invoked.")
+    {
+        constexpr std::optional<int> opt{};
+        SECTION("When algorithm is used via lvalue-ref overload.")
+        {
+            SCOPED_EXP action.expect_call()
+                and finally::returns(std::optional{42});
+            gimo::OrElseAlgorithm orElse{std::move(action)};
+            decltype(auto) result = orElse(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+
+        SECTION("When algorithm is used via const lvalue-ref overload.")
+        {
+            SCOPED_EXP std::as_const(action).expect_call()
+                and finally::returns(std::optional{42});
+            gimo::OrElseAlgorithm orElse{std::move(action)};
+            decltype(auto) result = std::as_const(orElse)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+
+        SECTION("When algorithm is used via rvalue-ref overload.")
+        {
+            SCOPED_EXP std::move(action).expect_call()
+                and finally::returns(std::optional{42});
+            gimo::OrElseAlgorithm orElse{std::move(action)};
+            decltype(auto) result = std::move(orElse)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+
+        SECTION("When algorithm is used via const rvalue-ref overload.")
+        {
+            SCOPED_EXP std::move(std::as_const(action)).expect_call()
+                and finally::returns(std::optional{42});
+            gimo::OrElseAlgorithm orElse{std::move(action)};
+            decltype(auto) result = std::move(std::as_const(orElse))(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(42 == result);
+        }
+    }
+
+    SECTION("When input has a value, the action is not invoked.")
+    {
+        gimo::OrElseAlgorithm orElse{std::move(action)};
+        constexpr std::optional opt{1337};
+
+        SECTION("When algorithm is used via lvalue-ref overload.")
+        {
+            decltype(auto) result = orElse(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(result);
+        }
+
+        SECTION("When algorithm is used via const lvalue-ref overload.")
+        {
+            decltype(auto) result = std::as_const(orElse)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(result);
+        }
+
+        SECTION("When algorithm is used via rvalue-ref overload.")
+        {
+            decltype(auto) result = std::move(orElse)(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(result);
+        }
+
+        SECTION("When algorithm is used via const rvalue-ref overload.")
+        {
+            decltype(auto) result = std::move(std::as_const(orElse))(opt);
+            STATIC_REQUIRE(std::same_as<std::optional<int>, decltype(result)>);
+            CHECK(result);
         }
     }
 }
