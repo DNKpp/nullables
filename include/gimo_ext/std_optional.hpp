ab20ce4f94ba5d258dc87748bd00020c90e11a3d
DNKpp <dnkpp2011@gmail.com>
2025-10-19 17:37:04 +0200

    feat: add std::optional integration

---
+++ include/gimo_ext/std_optional.hpp
@@ -0,0 +1,22 @@
+//          Copyright Dominic (DNKpp) Koepke 2025 - 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef GIMO_EXT_STD_OPTIONAL_HPP
+#define GIMO_EXT_STD_OPTIONAL_HPP
+
+#include "gimo/Common.hpp"
+
+#include <optional>
+
+template <typename T>
+struct gimo::traits<std::optional<T>>
+{
+    static constexpr auto null{std::nullopt};
+
+    template <typename V>
+    using rebind_value =  std::optional<V>;
+};
+
+#endif
