b2b773616487c7eabde3e8a7f142e284e5dee4fa
DNKpp <dnkpp2011@gmail.com>
2025-11-29 22:21:47 +0100

    feat: add gimo.hpp

---
+++ include/gimo.hpp
@@ -0,0 +1,22 @@
+//           Copyright Dominic (DNKpp) Koepke 2025
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef GIMO_HPP
+#define GIMO_HPP
+
+#include "gimo/Version.hpp"
+
+#include "gimo/Config.hpp"
+
+#include "gimo/Common.hpp"
+#include "gimo/Pipeline.hpp"
+
+#include "gimo/algorithm/Common.hpp"
+
+#include "gimo/algorithm/AndThen.hpp"
+#include "gimo/algorithm/OrElse.hpp"
+#include "gimo/algorithm/Transform.hpp"
+
+#endif
