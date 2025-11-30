e26364b07cffec0aa107eeb6004ca41fc570bcea
DNKpp <dnkpp2011@gmail.com>
2025-11-30 13:56:42 +0100

    fix: add missing concepts include

--- include/gimo/Common.hpp
+++ include/gimo/Common.hpp
@@ -1,13 +1,14 @@
-//          Copyright Dominic (DNKpp) Koepke 2025 - 2025.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          https://www.boost.org/LICENSE_1_0.txt)
+//           Copyright Dominic (DNKpp) Koepke 2025.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef GIMO_COMMON_HPP
 #define GIMO_COMMON_HPP
 
 #pragma once
 
+#include <concepts>
 #include <type_traits>
 #include <utility>
 
