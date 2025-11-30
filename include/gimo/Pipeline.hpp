c37a609a5e38ff5cda9e40402c1debf0a240bb3f
Dominic Koepke <koepke@aunovis.de>
2025-11-30 15:03:13 +0100

    fix: implement operator| in terms of Pipeline::append

--- include/gimo/Pipeline.hpp
+++ include/gimo/Pipeline.hpp
@@ -70,14 +70,14 @@ namespace gimo
         [[nodiscard]]
         friend constexpr auto operator|(Pipeline const& prefix, Pipeline<SuffixSteps...> suffix)
         {
-            return append(prefix, std::move(suffix).m_Steps);
+            return prefix.append(std::move(suffix));
         }
 
         template <typename... SuffixSteps>
         [[nodiscard]]
         friend constexpr auto operator|(Pipeline&& prefix, Pipeline<SuffixSteps...> suffix)
         {
-            return append(std::move(prefix), std::move(suffix).m_Steps);
+            return std::move(prefix).append(std::move(suffix));
         }
 
     private:
