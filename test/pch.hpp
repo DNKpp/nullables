546450358fa0a19a1cc2706c98c818259e3d9e9a
DNKpp <dnkpp2011@gmail.com>
2025-11-30 20:27:40 +0100

    chore: register mimicpp::print as CATCH_CONFIG_FALLBACK_STRINGIFIER

--- test/pch.hpp
+++ test/pch.hpp
@@ -1,11 +1,10 @@
-//          Copyright Dominic (DNKpp) Koepke 2025 - 2025.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          https://www.boost.org/LICENSE_1_0.txt)
+//           Copyright Dominic (DNKpp) Koepke 2025.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
-#include <catch2/catch_all.hpp>
 #include <mimic++/mimic++.hpp>
 #include <mimic++_ext/adapters/Catch2.hpp>
 
@@ -13,3 +12,5 @@ namespace finally = mimicpp::finally;
 namespace then = mimicpp::then;
 namespace expect = mimicpp::expect;
 namespace matches = mimicpp::matches;
+
+#include <catch2/catch_all.hpp>
